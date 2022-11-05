51ee83a87dab2a97e02cab1d8058f0654f154dfb
Genilton Souza <genilton.souza26@gmail.com>
2022-11-05 14:07:47 -0400

    feat(Prdocut) add product Schema

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -1,9 +1,58 @@
 const mongoose = require("mongoose");
 
 const productSchema = mongoose.Schema({
-  name: String,
-  image: String,
-  countInStock: Number,
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  richDescription: {
+    type: String,
+    default: "",
+  },
+  image: {
+    type: String,
+    default: "",
+  },
+  images: [
+    {
+      type: String,
+    },
+  ],
+  price: {
+    type: Number,
+    default: 0,
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Category",
+    required: true,
+  },
+  countInStock: {
+    type: Number,
+    required: true,
+    min: 0,
+    max: 99999,
+  },
+  rating: {
+    type: Number,
+    default: 0,
+  },
+  numberOfReviews: {
+    type: NUmber,
+    default: 0,
+  },
+  isFeatured: {
+    type: Boolean,
+    default: false,
+  },
+  dateCreated: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 exports.Product = mongoose.model("Product", productSchema);
