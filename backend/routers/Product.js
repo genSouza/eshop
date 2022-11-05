299679a2597b47d88f738ac84f173d27489ee2b6
Genilton Souza <genilton.souza26@gmail.com>
2022-11-05 18:27:03 -0400

    feat(product): add find product by id route

--- backend/routers/Product.js
+++ backend/routers/Product.js
@@ -17,12 +17,29 @@ router.get(`/`, async (req, res) => {
   }
 });
 
+router.get(`/:id`, async (req, res) => {
+  try {
+    const result = await Product.findById(req.params.id.toString().trim());
+
+    if (result) {
+      res.status(200).json({ success: true, result: result });
+    } else {
+      res.status(404).json({ success: false, message: "Id not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ success: false, error: error });
+  }
+});
+
 router.post(`/`, async (req, res) => {
   try {
     const category = await Category.findById(req.body.category);
-    
-    if(!category) return res.status(404).json({success: false, message: "Invalid Category" });
-    
+
+    if (!category)
+      return res
+        .status(404)
+        .json({ success: false, message: "Invalid Category" });
+
     const product = new Product({
       name: req.body.name,
       description: req.body.description,
@@ -37,7 +54,7 @@ router.post(`/`, async (req, res) => {
       isFeatured: req.body.isFeatured,
       dateCreated: req.body.dateCreated,
     });
-    
+
     const result = await product.save();
     if (result) {
       res.status(201).json({ success: true, result: result });
