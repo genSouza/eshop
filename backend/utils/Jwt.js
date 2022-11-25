ef96132b12609f477bbe95c9520654756d481f57
Genilton Souza <genilton.souza26@gmail.com>
2022-11-25 17:27:05 -0400

    feat(jwt): add revoke function

--- backend/utils/Jwt.js
+++ backend/utils/Jwt.js
@@ -2,12 +2,13 @@ require("dotenv/config");
 
 const { expressjwt: expressJwt } = require("express-jwt");
 const secret = process.env.SECRET;
-const api = process.env.API;
+const api = process.env.API_URL;
 
 function authJwt() {
   return expressJwt({
     secret,
     algorithms: ["HS256"],
+    isRevoked: isRevoked,
   }).unless({
     path: [
       { url: /\/api\/v1\/products(.*)/, methods: ["GET", "OPTIONS"] },
@@ -16,6 +17,12 @@ function authJwt() {
       `${api}/users/register`,
     ],
   });
+}
+
+async function isRevoked(req, token) {
+  if (!token.payload.isAdmin) {
+    return true;
+  }
 }
 
 module.exports = authJwt;
