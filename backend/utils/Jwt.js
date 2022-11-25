4e53eadbf139526c103d1c3fddcd8b89e35c466f
Genilton Souza <genilton.souza26@gmail.com>
2022-11-25 16:54:23 -0400

    feat(jwt): add jwt unless filter

--- backend/utils/Jwt.js
+++ backend/utils/Jwt.js
@@ -2,11 +2,19 @@ require("dotenv/config");
 
 const { expressjwt: expressJwt } = require("express-jwt");
 const secret = process.env.SECRET;
+const api = process.env.API;
 
 function authJwt() {
   return expressJwt({
     secret,
     algorithms: ["HS256"],
+  }).unless({
+    path: [
+      { url: /\/api\/v1\/products(.*)/, methods: ["GET", "OPTIONS"] },
+      { url: /\/api\/v1\/categories(.*)/, methods: ["GET", "OPTIONS"] },
+      `${api}/users/login`,
+      `${api}/users/register`,
+    ],
   });
 }
 
